0418f26c0a4b0d82a9dad13860b5c8b739f8b8ef
cormac12 <152636633+cormac12@users.noreply.github.com>
2024-04-30 19:36:28 -0400

    Add files via upload

--- enemy.py
+++ enemy.py
@@ -7,6 +7,8 @@ class Enemy:
     def __init__(self, x, y, vx, vy):
         self.angle = 0
 
+        self.my_font = pygame.font.SysFont('Arial', 15)
+
         self.original_image = pygame.image.load("spaceship2.png")
         self.display_image = pygame.transform.rotate(self.original_image, self.angle)
         self.display_image = pygame.transform.rotate(self.original_image, self.angle)
@@ -30,8 +32,8 @@ class Enemy:
         self.display_image = pygame.transform.rotate(self.original_image, self.angle)
 
     def accelerate(self):
-        self.vy -= math.cos(math.radians(self.angle)) * .0
-        self.vx -= math.sin(math.radians(self.angle)) * .0
+        self.vy -= math.cos(math.radians(self.angle)) * 0.05
+        self.vx -= math.sin(math.radians(self.angle)) * 0.05
 
     def update_pos(self):
         self.x += self.vx
@@ -39,7 +41,13 @@ class Enemy:
 
     def get_angle_to_player(self, player_x, player_y):
         if self.x > player_x and self.y > player_y:
-            return math.degrees(math.tan((self.x - player_y)/(self.y-player_y)))
+            return math.degrees(math.atan((self.x - player_x)/(self.y-player_y)))
+        elif self.x > player_x and self.y < player_y:
+            return math.degrees((math.atan((self.x - player_x)/(self.y-player_y)))) + 180
+        elif self.x < player_x and self.y < player_y:
+            return math.degrees((math.atan((self.x - player_x)/(self.y-player_y)))) + 180
+        elif self.x < player_x and self.y > player_y:
+            return math.degrees(math.atan((self.x - player_x)/(self.y-player_y)))
         else:
             return 0
 
@@ -47,3 +55,42 @@ class Enemy:
         self.angle = direction
         self.angle %= 360
         self.display_image = pygame.transform.rotate(self.original_image, self.angle)
+
+    def target_direction(self, target): # rotates the ship toward the player.
+        # returns true when pointing at the target
+        if -5 < self.angle - target < 5:
+            self.point(target)
+            self.cw = self.my_font.render(str((self.angle - target) % 360), True, (255, 255, 255))
+            self.ccw = self.my_font.render(str((self.angle + target) % 360), True, (255, 255, 255))
+            return True
+        elif (self.angle - target) % 360 <= (self.angle + target) % 360:
+            self.rotate("clockwise")
+        elif (self.angle - target) % 360 >= (self.angle + target) % 360:
+            self.rotate("counterclockwise")
+
+        self.cw = self.my_font.render(str((self.angle - target) % 360), True, (255, 255, 255))
+        self.ccw = self.my_font.render(str((self.angle + target) % 360), True, (255,255,255))
+        return False
+
+    def target_vector(self, direction, magnitude, player_vx, player_vy):
+        # rotates and accelerates craft in order to achieve target relative velocity vector to player
+        target_vx = magnitude * math.sin(math.radians(direction))
+        target_vy = magnitude * math.cos(math.radians(direction))
+        if self.vx > player_vx and self.vy > player_vy:
+            target_angle = math.degrees(math.atan((self.vx - player_vx)/(self.vy-player_vy)))
+        elif self.vx > player_vx and self.vy < player_vy:
+            target_angle = math.degrees((math.atan((self.vx - player_vx)/(self.vy-player_vy)))) + 180
+        elif self.vx < player_vx and self.vy < player_vy:
+            target_angle = math.degrees((math.atan((self.vx - player_vx)/(self.vy-player_vy)))) + 180
+        elif self.vx < player_vx and self.vy > player_vy:
+            target_angle = math.degrees(math.atan((self.vx - player_vx)/(self.vy-player_vy)))
+        else:
+            target_angle = 0
+
+        # self.target_direction(target_angle)
+        if (self.target_direction(target_angle) and
+                round(math.sqrt((self.vx-player_vx)**2 + (self.vy-player_vy)**2),0) < round(math.sqrt(target_vx**2 + target_vy**2),0)):
+            self.accelerate()
+
+
+
--- main.py
+++ main.py
@@ -29,14 +29,16 @@ camera_pos = (0,0)
 globals.globals_dict["player_x"] = p.x
 globals.globals_dict["player_y"] = p.y
 
-enemies = [Enemy(0, 0, 0, 0), Enemy(100, 100, 0, 0)]
+enemies = [ Enemy(100, 100, 0, 0)]
+
+fps = 30
 
 clock = pygame.time.Clock()
 
 # -------- Main Program Loop -----------
 while run:
 
-    clock.tick(30)
+    clock.tick(fps)
 
     keys = pygame.key.get_pressed()
 
@@ -46,6 +48,13 @@ while run:
         p.rotate("counterclockwise")
     if keys[pygame.K_w]:
         p.accelerate()
+    if keys[pygame.K_SPACE]:
+        fps = 1
+    if keys[pygame.K_UP]:
+        fps *= 1.5
+    if keys[pygame.K_DOWN]:
+        fps /= 1.5
+
 
     p.update_pos()
     # camera_pos = (p.x + p.rect.width/2 - 750, p.y + p.rect.height/2 - 500)
@@ -53,9 +62,10 @@ while run:
 
     for i in enemies:
         if random.randint(1,1) == 1:
-            i.rotate("clockwise")
-            i.accelerate()
-            i.point(i.get_angle_to_player(p.x, p.y))
+            # i.rotate("clockwise")
+            # i.accelerate()
+            # i.target_direction(i.get_angle_to_player(p.x, p.y))
+            i.target_vector(180, 1, p.vx, p.vy)
 
     for i in enemies:
         i.update_pos()
@@ -71,9 +81,9 @@ while run:
     # display_x = my_font.render(str(camera_pos[0]), True, (255,255,255))
     # display_y = my_font.render(str(camera_pos[1]), True, (255,255,255))
 
-    display_x = my_font.render(str(p.x), True, (255,255,255))
-    display_y = my_font.render(str(p.y), True, (255,255,255))
-
+    display_x = my_font.render(str(enemies[0].cw), True, (255,255,255))
+    display_y = my_font.render(str(enemies[0].ccw), True, (255,255,255))
+    display_fps = my_font.render(str(fps), True, (255,255,255))
     screen.fill((0, 0, 0))
     # ------Blit Zone Start------
 
@@ -83,8 +93,9 @@ while run:
         screen.blit(i.display_image, pygame.Rect(i.x - camera_pos[0] - i.display_image.get_width()/2,
                                                  i.y - camera_pos[1] - i.display_image.get_height()/2, i.display_image.get_width(),
                                                  i.display_image.get_height()))
-    screen.blit(display_x, (0, 0))
-    screen.blit(display_y, (0, 15))
+    screen.blit(enemies[0].cw, (0, 0))
+    screen.blit(enemies[0].ccw, (0, 15))
+    screen.blit(display_fps, (0, size[1]-30))
     # screen.blit(i, (100-p.x, 100-p.y))
 
     # ------Blit Zone End------
